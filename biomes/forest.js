6e7e9086b34ea02c262daf4ccf1910733d65ac79
khrome <abbey@khrome.net>
2018-01-21 12:37:31 -0800

    leaves on trees

--- biomes/forest.js
+++ biomes/forest.js
@@ -1,5 +1,20 @@
 var Generators = require('voxel-generators');
 
+function nextToTree(trees, x, z, tileSize){
+    return (
+        trees[(x+1) +  z   *tileSize] ||
+        trees[(x+1) + (z+1)*tileSize] ||
+        trees[ x    + (z+1)*tileSize] ||
+        // -
+        trees[(x-1) +  z   *tileSize] ||
+        trees[(x-1) + (z-1)*tileSize] ||
+        trees[ x    + (z-1)*tileSize] ||
+        // +/-
+        trees[(x+1) + (z-1)*tileSize] ||
+        trees[(x-1) + (z+1)*tileSize]
+    );
+}
+
 module.exports = {
     name : 'forest',
     rarity : 'common',
@@ -12,14 +27,34 @@ module.exports = {
                 trees[x + z*32] = rand < 1?Math.floor(11+context.random()*15):0;
             }
         }
+        var lower = 8;
+        var upper = 12;
         return Generators.SeamlessNoiseFactory(
             context.seed,
             Generators.Noise.perlin(context.random),
-            8, 12, function(x, y, z, value){
+            lower, upper, function(x, y, z, value){
                 if(value === 1) return 16;
                 var treeHeight = trees[x + z*32];
+                //the trunk
                 if(value === 0 && treeHeight && treeHeight >= y){
-                    return 19;
+                    if(treeHeight > y) return 22;
+                    if(treeHeight = y) return 25;
+                }
+                // slightly janky as we have to deterministicly pick a branch
+                // height without knowing the ground height ::crosses fingers::
+                var branchHeight = lower + Math.floor(
+                    context.random()*(upper-lower)
+                ) + 3;
+                treeHeight = nextToTree(trees, x, z, 32)
+                if(
+                    (value === 0) && //don't overwite voxels
+                    (treeHeight) && //there's a tree next to this pixel
+                    y > branchHeight &&
+                    y < treeHeight
+                ){
+                    var interval = Math.abs(Math.floor((treeHeight-branchHeight)/2));
+                    var test = (y-2) % interval === 0;// ~2 clumps per tree
+                    if(test) return 25;
                 }
                 return value;
             }
